2ace492d4fa1a145afbb3748031cac9528a2827e
HuntsmanRoym <grins008@morris.umn.edu>
2018-10-25 10:38:23 -0500

    Add beginning code for ExchangeRateReader.java - baseURL field

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 /**
  * Provide access to basic currency exchange rate services.
- * 
- * @author PUT YOUR TEAM NAME HERE
+ *
+ * @author mcdolanklenam
  */
 public class ExchangeRateReader {
+
+    private String baseURL;
 
     /**
      * Construct an exchange rate reader using the given base URL. All requests
@@ -16,12 +18,13 @@ public class ExchangeRateReader {
      * for specific days will be constructed from that URL by appending the
      * year, month, and day; the URL for 25 June 2010, for example, would be
      * http://api.finance.xaviermedia.com/api/2010/06/25.xml
-     * 
+     *
      * @param baseURL
      *            the base URL for requests
      */
     public ExchangeRateReader(String baseURL) {
         // TODO Your code here
+        this.baseURL = baseURL;
         /*
          * DON'T DO MUCH HERE!
          * People often try to do a lot here, but the action is actually in
@@ -33,7 +36,7 @@ public class ExchangeRateReader {
     /**
      * Get the exchange rate for the specified currency against the base
      * currency (the Euro) on the specified date.
-     * 
+     *
      * @param currencyCode
      *            the currency code for the desired currency
      * @param year
@@ -47,13 +50,17 @@ public class ExchangeRateReader {
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException {
         // TODO Your code here
+
+
+        URL url = new URL(baseURL/year-month-day);
+        InputStream inputStream = url.openStream();
         throw new UnsupportedOperationException();
     }
 
     /**
      * Get the exchange rate of the first specified currency against the second
      * on the specified date.
-     * 
+     *
      * @param fromCurrency
      *            the currency code we're exchanging *from*
      * @param toCurrency
